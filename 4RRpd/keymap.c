9cb5b6eec17b4e570e3c860532680977cebeda6d
Balint Szebenyi <szebenyib@gmail.com>
2025-01-01 13:14:28 +0100

    Commenting kc_mins

--- 4RRpd/keymap.c
+++ 4RRpd/keymap.c
@@ -1256,7 +1256,7 @@ bool caps_word_press_user(uint16_t keycode) {
         case HU_AA:
         case HU_II:
         case HU_OE:
-        case HU_UE:
+        /* case HU_UE: */
         /* case HU_Z: */
         /* case HU_Y: */
         case HU_UU:
